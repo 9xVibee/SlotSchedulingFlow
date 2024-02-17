a6d3942fc0b18f6b22ae5663b69a2e3b03014444
Abhay Panchal <abpanchal951357@gmail.com>
2024-02-17 09:25:55 +0530

    feat: Backend Setup done

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+const PORT = 3000;
+
+// middleware
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log("Server Started at port ", PORT);
+});
